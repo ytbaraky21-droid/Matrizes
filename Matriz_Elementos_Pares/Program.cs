30116b513cbb72631e6b87654a7c022a1a94e3d7
ytbaraky21-droid <ytbaraky21@gmail.com>
2025-10-23 21:49:47 -0300

    Matrizes

---
+++ Matriz_Elementos_Pares/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Definindo uma matriz 3x3 como exemplo
+        int[,] matriz = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        Console.WriteLine("Elementos pares da matriz:");
+
+        // Percorrendo a matriz
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                if (matriz[i, j] % 2 == 0)
+                {
+                    Console.WriteLine(matriz[i, j]);
+                }
+            }
+        }
+    }
+}
---
+++ Matrizes_Soma_Nprimos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class SomaPrimosMatriz
+{
+    // Função para verificar se um número é primo
+    static bool EhPrimo(int numero)
+    {
+        if (numero < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(numero); i++)
+        {
+            if (numero % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        // Exemplo de matriz
+        int[,] matriz = {
+            { 2, 4, 5 },
+            { 7, 10, 11 },
+            { 13, 16, 17 }
+        };
+
+        int somaPrimos = 0;
+
+        // Percorre a matriz e soma os números primos
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                if (EhPrimo(matriz[i, j]))
+                {
+                    somaPrimos += matriz[i, j];
+                }
+            }
+        }
+
+        Console.WriteLine("A soma dos números primos na matriz é: " + somaPrimos);
+    }
+}
+
